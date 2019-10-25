cd1a9185f25b5f75ed1be8e0e6689487600f2907
dmalinovschi <dmitrii.malinovschii@gmail.com>
2019-10-25 15:07:36 +0300

    - added dependenses

--- frontend/core/src/main/java/annotarions/ElementLocator.java
+++ frontend/core/src/main/java/annotarions/ElementLocator.java
@@ -1,4 +1,13 @@
 package annotarions;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.FIELD})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface ElementLocator {
+    String name();
+    String xpath() default "";
 }
--- frontend/core/src/main/java/annotarions/PageLocator.java
+++ frontend/core/src/main/java/annotarions/PageLocator.java
@@ -1,4 +1,13 @@
 package annotarions;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
 public @interface PageLocator {
+    String name() default "";
+    String url() default "/";
 }
